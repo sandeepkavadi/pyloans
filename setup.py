a520813a7f92975be8bbe4206cc68f2eba35f537
Sandeep <sandeep.kavadi.uc@gmail.com>
2022-12-24 02:43:13 -0500

    created initial project structure and worked out the bare-bones framework of the Loan class.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Package to generate analyze and work with simple installment loans.',
+    author='Sandeep Kavadi',
+    license='MIT',
+)
