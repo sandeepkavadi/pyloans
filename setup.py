c1f7d3b6fd9021660fcc417e558e24b6f298df1f
Sandeep <sandeep.kavadi.uc@gmail.com>
2022-12-24 17:31:24 -0500

    Re-organized folder structure. Laid down bare-bones structure for documentation.

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
-from setuptools import find_packages, setup
+from __future__ import annotations
+
+from setuptools import find_packages
+from setuptools import setup
 
 setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Package to generate analyze and work with simple installment loans.',
+    description="""Package to generate analyze and work with
+    simple installment loans.""",
     author='Sandeep Kavadi',
     license='MIT',
 )
